3000ed6e02a48045c1a9b8b322a32df3cf6f8b45
Sven Vinkemeier <svenvinkemeier@live.de>
2019-04-01 15:44:48 +0200

    Added tests for path conditions computation

---
+++ CryptoAnalysis/src/test/java/pathconditions/PathConditionsTests.kt
@@ -0,0 +1,116 @@
+package pathconditions
+
+import boomerang.jimple.Statement
+import crypto.pathconditions.computeRefinedSimplifiedPathConditions
+import crypto.pathconditions.expressions.WithContextFormat
+import crypto.pathconditions.expressions.toString
+import org.junit.Test
+import soot.Scene
+import soot.SootClass
+import soot.jimple.Stmt
+import javax.crypto.Cipher
+import kotlin.reflect.KFunction
+import kotlin.test.assertEquals
+
+class PathConditionsTests {
+
+    private val klass: SootClass
+
+    init {
+        setupSoot(this.javaClass.name)
+        klass = Scene.v().loadClass(this.javaClass.name, SootClass.BODIES)
+    }
+
+    /**
+     * Given a set of relevant statements, tests whether the correct path conditions are computed.
+     * @param relevantLines The line numbers (with 0 referring to the first statement in [testMethod]) of relevant statements
+     * @param expectedConditions Expected conditions as Strings
+     * @param testMethod The method to be analyzed
+     */
+    fun test(relevantLines: Set<Int>, expectedConditions: Set<String>, testMethod: KFunction<kotlin.Unit>) {
+        val method = klass.methods.single { it.name == testMethod.name }
+        val units = method.retrieveActiveBody().units
+
+        val firstLine = units
+            .map { it.lineNumber() }
+            .first { it >= 0 }
+
+        val relevantStatements = units
+            .filter { it.lineNumber() - firstLine in relevantLines }
+            .map { Statement(it as Stmt, method) }
+
+        val conditions = computeRefinedSimplifiedPathConditions(relevantStatements)
+
+        val conditionsAsString = conditions
+            .map { it.condition.toString(WithContextFormat.ContextFree) }
+            .toSet()
+
+        assertEquals(expectedConditions.toSet(), conditionsAsString,
+            "Actual path conditions do not match expected ones")
+    }
+
+
+    private fun simpleBranching(i: Int) {
+        if (i > 0) {
+            if (i < 5) {
+                nop()
+            } else {
+                nop()
+            }
+        } else {
+            nop()
+        }
+    }
+
+    @Test
+    fun simpleBranchingTest1() = test(
+        setOf(2),
+        setOf("\$param0 > 0L && 5L > \$param0"),
+        ::simpleBranching
+    )
+
+    @Test
+    fun simpleBranchingTest2() = test(
+        setOf(4),
+        setOf("\$param0 >= 5L"), // implies "$param0 > 0"
+        ::simpleBranching
+    )
+
+    @Test
+    fun simpleBranchingTest3() = test(
+        setOf(7),
+        setOf("\$param0 <= 0L"),
+        ::simpleBranching
+    )
+
+    @Test
+    fun simpleBranchingTest4() = test(
+        setOf(4, 7),
+        setOf("false"),
+        ::simpleBranching
+    )
+
+
+    private fun multiAssignment() {
+        var x = "asdf"
+
+        if (Math.random() < .5)
+            x = "no"
+
+        val c = Cipher.getInstance(x)
+    }
+
+    @Test
+    fun multiAssignmentTest1() = test(
+        setOf(3, 5),
+        setOf("Math.random() < 0.5"),
+        ::multiAssignment
+    )
+
+    @Test
+    fun multiAssignmentTest2() = test(
+        setOf(0, 5),
+        setOf("Math.random() >= 0.5"),
+        ::multiAssignment
+    )
+}
---
+++ CryptoAnalysis/src/test/java/pathconditions/Utils.kt
@@ -0,0 +1,63 @@
+package pathconditions
+
+import crypto.pathconditions.ofType
+import soot.G
+import soot.Scene
+import soot.SootClass
+import soot.Unit
+import soot.options.Options
+import soot.tagkit.LineNumberTag
+import java.io.File
+
+fun getSootClassPath(): String {
+    val classPaths = arrayOf(
+        System.getProperty("user.dir") + File.separator + "target" + File.separator + "classes",
+        System.getProperty("user.dir") + File.separator + "target" + File.separator + "test-classes"
+    )
+    return classPaths
+        .filter { File(it).exists() }
+        .joinToString(";")
+//        ?: throw RuntimeException("Classpath could not be found")
+}
+
+fun setupSoot(mainClass: String? = null) {
+    G.v()
+    G.reset()
+
+    Options.v().apply {
+        set_whole_program(true)
+        set_keep_line_number(true)
+        setPhaseOption("cg.spark", "on")
+        set_output_format(Options.output_format_none)
+        set_no_bodies_for_excluded(true)
+        set_allow_phantom_refs(true)
+        set_include(
+            listOf(
+                "java.lang.*",
+                "java.util.*",
+                "java.io.*",
+                "sun.misc.*",
+                "java.net.*",
+                "javax.servlet.*",
+                "javax.crypto.*"
+            )
+        )
+        setPhaseOption("jb", "use-original-names:true")
+        set_soot_classpath(getSootClassPath())
+        set_prepend_classpath(true)
+        // set_main_class(this.getTargetClass())
+    }
+
+    Scene.v().loadNecessaryClasses()
+
+    if (mainClass != null) {
+        val klass = Scene.v().forceResolve(mainClass, SootClass.BODIES)
+        klass.setApplicationClass()
+        klass.methods.forEach(::println)
+    }
+}
+
+fun Unit.lineNumber() =
+    tags.ofType<LineNumberTag>().firstOrNull()?.lineNumber ?: -1
+
+fun nop() {}
